5465241878b5255e50085243a18407ebac3a97cb
unknown <abhishekrock796@gmail.com>
2024-10-05 12:44:18 +0530

    header and routes are configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+
+function AppLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet /> {/* This is where the child routes will render */}
+    </>
+  );
+}
 
 function App() {
-  return <>
-  <div className="bg-green-400 text-white text-center">Hello there Welcome to realEstate world !
-  </div>
-  </>;
+  const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <AppLayout />, // Header will be included for all child routes
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: 'login',
+          element: <Login />
+        },
+        {
+          path: 'profile',
+          element: <Profile />
+        },
+        {
+          path: 'signup',
+          element: <SignUp />
+        }
+      ]
+    }
+  ]);
+
+  return <RouterProvider router={routes}></RouterProvider>;
 }
 
 export default App;
